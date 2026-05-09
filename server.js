ebf94ab81838535a55f4fabafc646dbc2d6eff3a
NguyenQuocNhat294 <nqn02092004@gmail.com>
2026-05-09 08:40:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
    'https://ftrui-trace-fe.vercel.app',
+    'https://fruit-trace-fe.vercel.app',
 ];
 
 app.use(
